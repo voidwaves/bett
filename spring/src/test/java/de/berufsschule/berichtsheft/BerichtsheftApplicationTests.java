764460ba5acc387bda5510dc98c3dd77bb0fcd79
Maddin <martin.manka@gmx.de>
2020-08-07 12:01:35 +0200

    added spring structure

---
+++ spring/src/test/java/de/berufsschule/berichtsheft/BerichtsheftApplicationTests.java
@@ -0,0 +1,13 @@
+package de.berufsschule.berichtsheft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BerichtsheftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
