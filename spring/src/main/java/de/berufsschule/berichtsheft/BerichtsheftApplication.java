9ca7710ccfea666052a9522f0f9d089613b5356d
Maddin <martin.manka@gmx.de>
2020-08-07 12:01:35 +0200

    added spring structure

---
+++ spring/src/main/java/de/berufsschule/berichtsheft/BerichtsheftApplication.java
@@ -0,0 +1,13 @@
+package de.berufsschule.berichtsheft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BerichtsheftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BerichtsheftApplication.class, args);
+	}
+
+}
