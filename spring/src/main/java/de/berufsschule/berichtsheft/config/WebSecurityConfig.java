5bd8ca093d0126aaa7015d45c9df15242940818a
--global <j.hellwig@eos-ts.com>
2020-08-21 12:57:27 +0200

    fixed cors error

--- spring/src/main/java/de/berufsschule/berichtsheft/config/WebSecurityConfig.java
+++ spring/src/main/java/de/berufsschule/berichtsheft/config/WebSecurityConfig.java
@@ -50,7 +50,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
 
-        httpSecurity.csrf().disable()
+        httpSecurity.cors().and().csrf().disable()
                 .authorizeRequests().antMatchers("/authenticate", "/register").permitAll()
                 .anyRequest().authenticated()
                 .and().exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint)
