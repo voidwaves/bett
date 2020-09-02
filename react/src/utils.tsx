4a50db6d22600316a6d13977b952422036759bb5
--global <j.hellwig@eos-ts.com>
2020-09-02 10:07:19 +0200

    added some utillity functions

--- react/src/utils.tsx
+++ react/src/utils.tsx
@@ -1,6 +1,6 @@
 
 import { format, parse } from 'ts-date'
-import { SetStateAction, ChangeEvent } from 'react'
+import { ChangeEvent, Dispatch } from 'react'
 
 const pattern = 'YYYY-MM-DD'
 
@@ -8,6 +8,29 @@ export const dateToString = (date: Date): string => format(date, pattern) as str
 
 export const stringToDate = (dateString: string): Date => parse(dateString, pattern) as Date
 
-export const fromEvent = function <Type>(setter: SetStateAction<any>) {
+export const isEqual = (first: Date, second: Date): boolean => {
+    return first.getTime() === second.getTime()
+}
+
+export const isEarlier = (first: Date, second: Date): boolean => {
+    return first.getTime() < second.getTime()
+}
+
+export const isLater = (first: Date, second: Date): boolean => {
+    return first.getTime() > second.getTime()
+}
+
+export const isInRange = (target: Date, start: Date, end: Date) => {
+    const [targetTime, startTime, endTime] = [target, start, end]
+    .map(date => date.getTime())
+    return targetTime >= startTime && targetTime <= endTime
+}
+
+export const dateRange = (start: Date, end: Date): Date[] => {
+    const newStart = new Date(start.getFullYear(), start.getMonth(), start.getDate() + 1)
+    return isEqual(start, end) ? [end] : [start, ...dateRange(newStart, end)]
+}
+
+export const fromEvent = function (setter: Dispatch<string>) {
     return (event: ChangeEvent<HTMLInputElement>) => setter(event.target.value)
 }
