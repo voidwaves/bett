d5f05bb2981db6610a0f1e69e2c9184f53685513
--global <j.hellwig@eos-ts.com>
2020-09-07 16:02:56 +0200

    rebased from master, solved merge conflicts

--- react/src/Types.tsx
+++ react/src/Types.tsx
@@ -1,84 +1,86 @@
-import ReportEntries from "./view/EntryList";
+import ReportEntries from "./view/EntryList"
 
-type Modify<T, R> = Omit<T, keyof R> & R;
+type Modify<T, R> = Omit<T, keyof R> & R
 
 export namespace App {
   export type ReportEntry = Modify<
     ApiResponse.ReportEntry,
     {
-      reportDate: Date;
-      exists: boolean;
+      reportDate: Date
+      exists: boolean
     }
-  >;
+  >
 
   export type User = Modify<
     ApiResponse.User,
     {
-      beginOfApprenticeship: Date;
+      beginOfApprenticeship: Date
     }
-  >;
+  >
 }
 
 export namespace ApiResponse {
   export type ReportEntry = {
-    id: number;
-    user: User;
-    content: string;
-    reportDate: string;
-    workingHours: number;
-    department: string;
-  };
+    id: number
+    user: User
+    content: string
+    reportDate: string
+    workingHours: number
+    department: string
+  }
 
   export type User = {
-    id: number;
-    firstName: string;
-    lastName: string;
-    beginOfApprenticeship: string;
-    label: string;
-    username: string;
-    password: string;
-  };
+    id: number
+    firstName: string
+    lastName: string
+    beginOfApprenticeship: string
+    label: string
+    username: string
+    password: string
+  }
 }
 
 export namespace ApiRequest {
   export namespace ReportEntry {
     export type Put = Post & {
-      id: number;
-    };
+      id: number
+    }
     export type Post = {
-      content: string;
-      reportDate: string;
-      workingHours: number;
-      department: string;
-    };
+      content: string
+      reportDate: string
+      workingHours: number
+      department: string
+    }
     export type Params = {
       params: {
-        start: string;
-        end: string;
-      };
-    };
+        start: string
+        end: string
+      }
+    }
   }
   export namespace User {
     export type Put = Post & {
-      id: number;
-    };
+      id: number
+    }
     export type Post = {
-      firstName: string;
-      lastName: string;
-      beginOfApprenticeship: string;
-      label: string;
-    };
+      firstName: string
+      lastName: string
+      beginOfApprenticeship: string
+      label: string
+      username: string
+      password: string
+    }
   }
   export type Login = {
-    username: string;
-    password: string;
-  };
+    username: string
+    password: string
+  }
   export type Register = {
-    username: string;
-    password: string;
-    firstName: string;
-    lastName: string;
-    label: string;
-    beginOfApprenticeship: string;
-  };
+    username: string
+    password: string
+    firstName: string
+    lastName: string
+    label: string
+    beginOfApprenticeship: string
+  }
 }
